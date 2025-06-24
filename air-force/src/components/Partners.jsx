76587b5bfd37c5a19688bd8315b6608dad813348
Vahe <ohanyanv703@gmail.com>
2025-06-25 01:22:55 +0400

    fully fixed marquee

--- air-force/src/components/Partners.jsx
+++ air-force/src/components/Partners.jsx
@@ -1,21 +1,29 @@
 const logos = [ 
-    {name: "BlueOrigin.png",width: 160},
-    {name: "Boeing.png",width: 140},
-    {name: "Darpa.png",width: 140},
-    {name: "Nasa.png",width: 120},
+    // {name: "BlueOrigin.png",width: 160},
+    {name: "Boeing.png",width: 150},
+    {name: "Aws.png",width:130},
     {name: "SpaceX.png",width:140},
+    {name: "Darpa.png",width: 150},
+    {name: "Nasa.png",width: 120},
+    {name: "Raytheon.png",width:150},
 ]
 export const PartnerMarquee = () => {
     return (
         <div className="partner-section">
             <h2>Powered by Industry Leaders</h2>
+            <div className="marquee-container">
             <div className="logo-marquee">
-                {console.log(logos)}
-                {[...logos,...logos].map((logo,index) => {
+                {[...logos,...logos,...logos,...logos].map((logo,index) => {
                     return (
-                        <img src={`/src/assets/logos/${logo.name}`}  alt={logo.name.split('.')[0]} key={index} width={logo.width} height={60}/>
+                        <img src={`/src/assets/logos/${logo.name}`}  
+                        alt={logo.name.split('.')[0]} 
+                        key={index}
+                        width={logo.width}
+                        height={60}
+                        className="marquee-logo"/>
                     )
                 })}
+            </div>
             </div>
         </div>
     )
