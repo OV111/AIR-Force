3a9c9a71b963458127df96a9352d08aef917e7af
Vahe <ohanyanv703@gmail.com>
2025-06-25 01:22:24 +0400

    Card react file

--- air-force/src/components/AircraftShowcase.jsx
+++ air-force/src/components/AircraftShowcase.jsx
@@ -1,21 +1,22 @@
 export const AirCraftCards = () => {
     const Aircrafts = [
-        {id:1, name: "F-22 Raptor", video: ""},
-        {id:2, name: "B-2 Spirit", video: "src/assets/B-2-Spirit.mp4"},
-        {id:3, name: "SR-71 Blackbird", video: ""},
+        {id:1, name: "F-22 Raptor", video: "src/assets/B-2-Spirit.mp4", info: "some text that will be printed"},
+        {id:2, name: "B-2 Spirit", video: "src/assets/B-2-Spirit.mp4", info: "some text that will be printed"},
+        {id:3, name: "SR-71 Blackbird", video: "src/assets/B-2-Spirit.mp4", info: "some text that will be printed"},
+        // {id:4, name: "F-35 Lightning II", video: "src/assets/B-2-Spirit.mp4", info: ""},
     ]
     return (
          <div className="aircraftCards">
                 {Aircrafts.map((aircraft) => {
                     return (
                         <div className="aircraftCard" key={aircraft.id}>
-                            <video className="card-background-video" autoPlay muted playsInline loop width={200} height={200}>
+                            <video className="card-background-video" autoPlay muted playsInline loop>
                                 <source src={aircraft.video} />
                             </video>
 
                             <div className="card-content">
                                 <h2>{aircraft.name}</h2>
-
+                                <p>{aircraft.info}</p>
                             </div>
                         </div>
                     )
